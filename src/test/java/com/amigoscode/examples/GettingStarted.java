308105ffdb2a67abe322a34fe6b98737edc721de
Bartosz <koston178@gmail.com>
2024-06-17 19:35:03 +0200

    gettingStarted changes

--- src/test/java/com/amigoscode/examples/GettingStarted.java
+++ src/test/java/com/amigoscode/examples/GettingStarted.java
@@ -22,5 +22,10 @@ public class GettingStarted {
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        people.stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .toList()
+                .forEach(System.out::println);
     }
 }
